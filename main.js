6dbf5a6f50d075a8dc8983a709451df3bc182087
TabCodes <tab.codes@gmail.com>
2017-12-25 11:15:02 -0500

    created the markup for the form

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log("hello");
+var madlib = `A vacation is when you take a trip to some ________________ place
+with your ________________ family. Usually you go to some place
+that is near a/an ________________ or up on a/an ________________.
+A good vacation place is one where you can ride ________________
+or play ________________ or go hunting for ________________`;
+
+var adjective1 = "";
+var adjective2 = "";
+var noun1 = "";
+var noun2 = "";
+var pluralNoun1 = "";
+var pluralNoun2 = "";
+var game = "";
